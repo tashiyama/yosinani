1d00137c8e2e4851303bdfa8cbb15829266ea756
tashiyama <mototo8201@gmail.com>
2022-06-03 16:37:06 +0900

    Update app.py

--- app.py
+++ app.py
@@ -9,7 +9,7 @@ from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 import os
-import wikipedia # 追加
+import wikipedia
 
 app = Flask(__name__)
 
@@ -19,7 +19,7 @@ YOUR_CHANNEL_SECRET = os.environ["YOUR_CHANNEL_SECRET"]
 line_bot_api = LineBotApi(YOUR_CHANNEL_ACCESS_TOKEN)
 handler = WebhookHandler(YOUR_CHANNEL_SECRET)
 
-wikipedia.set_lang("ja") # 追加
+wikipedia.set_lang("ja")
 
 @app.route("/callback", methods=['POST'])
 def callback():
@@ -34,21 +34,16 @@ def callback():
     return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
-# 先ほどの関数内にWikipediaのライブラリを使った処理を記述していく
 def handle_message(event):
     send_message = event.message.text
-    # 正常に検索結果が返った場合
     try:
         wikipedia_page = wikipedia.page(send_message)
-        # wikipedia.page()の処理で、ページ情報が取得できれば、以下のようにタイトル、リンク、サマリーが取得できる。
         wikipedia_title = wikipedia_page.title
         wikipedia_url = wikipedia_page.url
         wikipedia_summary = wikipedia.summary(send_message)
         reply_message = '【' + wikipedia_title + '】\n' + wikipedia_summary + '\n\n' + '【詳しくはこちら】\n' + wikipedia_url
-    # ページが見つからなかった場合
     except wikipedia.exceptions.PageError:
         reply_message = '【' + send_message + '】\nについての情報は見つかりませんでした。'
-    # 曖昧さ回避にひっかかった場合
     except wikipedia.exceptions.DisambiguationError as e:
         disambiguation_list = e.options
         reply_message = '複数の候補が返ってきました。以下の候補から、お探しの用語に近いものを再入力してください。\n\n'
